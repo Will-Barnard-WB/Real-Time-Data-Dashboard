aebe0841ed007849d37178183c6dedd0caa74eb8
Will-Barnard-WB <141440778+Will-Barnard-WB@users.noreply.github.com>
2025-02-28 00:07:15 +0000

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,41 +1,81 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 import psycopg2
 import os
+from datetime import datetime
+import logging
 
 app = Flask(__name__)
+
 CORS(app)
 
-# Database connection details
-DB_HOST = os.getenv('DB_HOST', 'db')  # Change if necessary
-DB_NAME = os.getenv('DB_NAME', 'postgres')
-DB_USER = os.getenv('DB_USER', 'airflow')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'airflow')
+
+DB_HOST = "postgres"
+DB_NAME = "postgres"
+DB_USER = "airflow"
+DB_PASS = "airflow"
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD
-    )
-    return conn
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS,
+            host=DB_HOST
+        )
+        return conn
+    except Exception as e:
+        print(f"Database connection error: {e}")
+        return None
 
-@app.route('/')
-def home():
-    return "Welcome to the home page"
+
+@socketio.on('connect')
+def handle_connect():
+    print("A client has connected")
+
+    emit('stock_update', {'message': 'Welcome to the stock updates WebSocket!'})
 
 
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("A client has disconnected")
 
-@app.route('/api/stocks', methods=['GET'])
-def get_stocks():
+
+@socketio.on('request_stock_data')
+def handle_request_stock_data():
+
     conn = get_db_connection()
+    if conn is None:
+        emit('stock_update', {'error': "Could not connect to database"})
+        return
+    
     cur = conn.cursor()
-    cur.execute('SELECT * FROM stock_data;')
-    stocks = cur.fetchall()
-    cur.close()
-    conn.close()
-    return jsonify(stocks)
+    try:
+        cur.execute("SELECT * FROM stock_data;")
+        rows = cur.fetchall()
+        
+        stocks = []
+        for row in rows:
+            stock = {"name": row[0].isoformat(), "symbol": row[1], "open": row[2], "close": row[3]}
+            stocks.append(stock)
+        
+
+        emit('stock_update', {'stocks': stocks})
+    except Exception as e:
+        emit('stock_update', {'error': f"Error fetching data: {str(e)}"})
+    finally:
+        cur.close()
+        conn.close()
+
+
+@app.route('/push_update')
+def push_update():
+    app.logger.info("Recived request to /push_update endpoint.")
+ 
+    handle_request_stock_data()
+    return "Update sent to clients"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5050)
